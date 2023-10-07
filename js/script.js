b524d1da43ab02409a3931564af899e6dde2ef8c
raffzhm <rafiazhim.ra@gmail.com>
2023-10-07 19:06:31 +0700

    change method js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 document.getElementById('lihatProposalBtn').addEventListener('click', function() {
   // Arahkan pengguna ke proposal.html
-  window.location.href = './pages/proposal.html';
+  window.location.href = './pages/page.html';
 });
